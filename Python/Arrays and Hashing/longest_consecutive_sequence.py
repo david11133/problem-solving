ae387b3d4738b7d99edc6d568c63fb9ee8e6130d
david11133 <davidnady4yad@gmail.com>
2025-02-27 10:16:12 +0200

    lingestConsecutive

--- Python/Arrays and Hashing/longest_consecutive_sequence.py
+++ Python/Arrays and Hashing/longest_consecutive_sequence.py
@@ -1,23 +1,19 @@
 def longest_consecutive(nums):
 
-    if not nums:
-        return 0
-        
-    num_set = set(nums)
-    longest_streak = 0
+    if not nums:
+        return 0
+    num_set = set(nums)
+    lingest_streak = 0
+    for num in num_set:
+        if num - 1 not in num_set:
+            current_num = num
+            current_streak = 1
+            while current_num + 1 in num_set:
+                current_num += 1
+                current_streak += 1
+            lingest_streak = max(lingest_streak, current_streak)
+    return lingest_streak
 
-    for num in num_set:
-        # Check if it's the start of a sequence
-        if num - 1 not in num_set:
-            current_num = num
-            current_streak = 1
-            # Count the consecutive numbers
-            while current_num + 1 in num_set:
-                current_num += 1
-                current_streak += 1
-            longest_streak = max(longest_streak, current_streak)
-    return longest_streak
-
-# Example usage:
-nums = [100, 4, 200, 1, 3, 2]
-print(longest_consecutive(nums))  # Output: 4
+print(longest_consecutive([100, 4, 200, 1, 3, 2])) # 4
+print(longest_consecutive([0,3,7,2,5,8,4,6,0,1])) # 9
+print(longest_consecutive([1,2,0,1])) # 3
