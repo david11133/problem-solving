33dd3a100bbe837a8b6aa78bb4a123db7f052142
david11133 <davidnady4yad@gmail.com>
2025-02-27 10:01:13 +0200

    groupAnagrams

--- Python/Arrays and Hashing/group_anagrams.py
+++ Python/Arrays and Hashing/group_anagrams.py
@@ -1,10 +1,13 @@
 from collections import defaultdict
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = defaultdict(list)
 
-        for s in strs:
-            sorted_str = ''.join(sorted(s))
-            anagrams[sorted_str].append(s)
-        return anagrams.values()
+        for s in strs:
+            sorted_str = ''.join(sorted(s))
+            anagrams[sorted_str].append(s)
+        return anagrams.values()
+
+sol = Solution()
+print(sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])) # [["eat","tea","ate"],["tan","nat"],["bat"]]
