9f3a8b7f75e05c145d328c3e8ef60529a5006452
david11133 <davidnady4yad@gmail.com>
2025-02-28 09:47:57 +0200

    twoSum

--- Python/Arrays and Hashing/two_sum.py
+++ Python/Arrays and Hashing/two_sum.py
@@ -1,8 +1,14 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums) 
-        for i in range(n): 
-	        for j in range(i + 1, n): 
-		        if nums[i] + nums[j] == target: 
-			        return [i, j] 
-		return None # Return None if no solution is found
+def twoSum(nums, target):
+	num_map = {}
+
+	for i, num in enumerate(nums):
+		complement = target - num
+
+		if complement in num_map:
+			return [num_map[complement], i]
+
+		num_map[num] = i	
+
+print(twoSum([2, 7, 11, 15], 9)) # [0, 1]
+print(twoSum([3, 2, 4], 6)) # [1, 2]
+print(twoSum([3, 3], 6)) # [0, 1]
