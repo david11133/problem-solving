aaf3a1d1ec68156056e593f626476483d887c648
david11133 <davidnady4yad@gmail.com>
2025-03-03 09:17:38 +0200

    combinationSum

--- Python/Backtracking/Combination_Sum.py
+++ Python/Backtracking/Combination_Sum.py
@@ -1,18 +1,21 @@
 def combinationSum(candidates, target):
     result = []
-    
-    def backtrack(start, target, path):
+
+    def backtrack(start, target, combination_list):
         if target == 0:
-            result.append(path)
+            result.append(list(combination_list))
+            return 
+
+        if target < 0:
             return
-
+        
         for i in range(start, len(candidates)):
-            if candidates[i] > target:
-                break
-            backtrack(i, target - candidates[i], path + [candidates[i]])
-
-    candidates.sort()
+            combination_list.append(candidates[i])
+            backtrack(i, target - candidates[i], combination_list)
+            combination_list.pop()
 
     backtrack(0, target, [])
-    
     return result
+
+print(combinationSum([2, 3, 6, 7], 7)) # [[2, 2, 3], [7]]
+print(combinationSum([2, 3, 5], 8)) # [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
