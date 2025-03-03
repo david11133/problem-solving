dec9ef3ee91c67aaa69b4a65184f4e184142374e
david11133 <davidnady4yad@gmail.com>
2025-03-03 09:23:10 +0200

    find minimum in a rotated sorted array

--- Python/Binary-Search/Find_minimum_in_a_rotated_sorted_array.py
+++ Python/Binary-Search/Find_minimum_in_a_rotated_sorted_array.py
@@ -1,12 +1,14 @@
 def findMin(nums):
-    left = 0
-    right = len(nums) - 1
+    left, right = 0, len(nums) - 1
 
-    while left < right:
-        mid = (left + right) // 2
-        if nums[mid] > nums[right]:
-            left = mid + 1
-        else:
-            right = mid
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+    
+    return nums[left]
 
-    return nums[left]
+print(findMin([3,4,5,1,2])) # 1
+print(findMin([4,5,6,7,0,1,2])) # 0
